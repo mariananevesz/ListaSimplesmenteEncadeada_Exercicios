17131ee0fb273d2d3a52317f945f3e472d8d4557
mariananevesz <nevespereiramariana9@gmail.com>
2026-06-07 16:52:02 -0300

    Exercício 2 - b

--- Exercicio2/ListaFuncionario.cs
+++ Exercicio2/ListaFuncionario.cs
@@ -24,5 +24,19 @@ public class ListaFuncionario
         }
     }
 
-    
+    public void inserirFim(string nome, int idade, string telefone, double salario)
+    {
+        NoFuncionario novoNo = new NoFuncionario(nome, idade, telefone, salario);
+        if (this.inicio == null)
+        {
+            this.inicio = novoNo;
+            this.fim = novoNo;
+        }
+        else
+        {
+            this.fim.prox = novoNo;
+            this.fim = novoNo;
+        }
+    }
+
 }
